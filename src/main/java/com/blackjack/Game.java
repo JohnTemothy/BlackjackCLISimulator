e4221b2e7b3ddc38b32229825bf339c189641dc5
JohnTemothy <126853924+JohnTemothy@users.noreply.github.com>
2025-05-29 23:51:31 +0800

    edited game.java and playerlist.java
    added checkBlackjack function and edited startGame function
    added custom username input in initgame function
    in Game.java
    
    added isBlackjackPlayerList and isBustPlayerList function in PlayerList.java

--- src/main/java/com/blackjack/Game.java
+++ src/main/java/com/blackjack/Game.java
@@ -1,5 +1,9 @@
 package com.blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import com.blackjack.models.Player;
 import com.blackjack.models.PlayerList;
 import com.blackjack.models.Shoe;
@@ -21,13 +25,54 @@ public class Game {
     public void initGame() {
         shoe = new Shoe(2);
         playerList = new PlayerList();
-        playerList.addPlayer(new Player("Name", false));
+
+        Scanner playerNameScanner = new Scanner(System.in);  // Allow for user input to change their username
+        System.out.println("Enter username");
+        String playerName = playerNameScanner.nextLine(); 
+
+        playerList.addPlayer(new Player(playerName, false)); //Maybe make like a limit or checks incase username is too long or username is nothing
         playerList.addPlayer(new Player("Dealer", true));
+
+        playerNameScanner.close();
     }
+
+    public Boolean checkBlackjack() {
+
+        //creates list of players with hand value > 21
+        List<String> listOfBlackjacks = playerList.isBlackjackPlayerList();
+
+        // if there is a single blackjack, print out winner
+        // TODO: make a function for this so that you can call this multiple times, maybe make a loop or something if the player/dealer keeps drawing
+        if (listOfBlackjacks.size() == 1) {
+            System.out.println(listOfBlackjacks.get(0) + "wins!");
+            return true;
+        }
+
+
+        // if there are more than one blackjacks, print out tie.
+        // TODO: make it so this works for more than 2 players.
+        else if (listOfBlackjacks.size() > 1) {
+            System.out.println("tie!");
+            return true;
+        }
+
+        return false;
+    }
+
 
     public void startGame() {
         io.println("Welcome to Blackjack");
         initGame();
+
+        // everything from here to the next comment is stuff i might move to a different function maybe
+
+        shoe.shuffle();
+        playerList.dealCards(shoe);
+
+        if (checkBlackjack()) {
+            return;
+        }
+        //
 
         io.println(shoe.toString());
     }
